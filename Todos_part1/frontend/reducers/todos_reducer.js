b12c47602adf9391bbbecea6d006ea678abf2417
yohan oh <yohan.oh332@gmail.com>
2021-03-04 16:17:27 -0500

    working phase 2

---
+++ Todos_part1/frontend/reducers/todos_reducer.js
@@ -0,0 +1,10 @@
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO, TODO_ERROR } from '../actions/todo_actions';
+
+const todosReducer = (state = {}, action) => {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  };
+  
+  export default todosReducer;
