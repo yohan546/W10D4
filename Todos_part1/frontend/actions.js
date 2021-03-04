c1a4834630406e1a9897d6fb9db96a391fdf016e
yohan oh <yohan.oh332@gmail.com>
2021-03-04 13:46:42 -0500

    first commit

---
+++ Todos_part1/frontend/actions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<Root/>, document.getElementById('main'))
+});
