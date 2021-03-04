2b96496c1b5596eef87ef03ed4853bae36797e35
yohan oh <yohan.oh332@gmail.com>
2021-03-04 13:55:20 -0500

    edit file structure

--- Todos_part1/frontend/actions.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<Root/>, document.getElementById('main'))
-});
