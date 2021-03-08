9466922a8b4c945a770172d96ac092b5a7b6fdb3
yohan oh <yohan.oh332@gmail.com>
2021-03-08 17:29:34 -0500

    testing phase 1

--- Todos/app/controllers/api/todos_controller.rb
+++ Todos/app/controllers/api/todos_controller.rb
@@ -1,5 +1,5 @@
 class Api::TodosController < ApplicationController
-
+  before_action :deny_access_if_not_logged_in
     def index 
         render json: Todo.all.where(user_id: current_user.id)
     end
--- Todos/config/routes.rb
+++ Todos/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 
   namespace :api, defaults: { format: :json } do 
     resources :todos, only: [:index, :show, :create, :destroy, :update]
