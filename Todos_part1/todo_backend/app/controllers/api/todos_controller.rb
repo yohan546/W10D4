448d684d31ce55d004e4df9373d05b644eea8852
yohan oh <yohan.oh332@gmail.com>
2021-03-08 17:01:50 -0500

    made routes for todo_controller

---
+++ Todos_part1/todo_backend/app/controllers/api/todos_controller.rb
@@ -0,0 +1,40 @@
+class Api::TodosController < ApplicationController
+
+    def index 
+        render json: Todo.all.where(user_id: current_user.id)
+    end
+    
+    def show 
+        render json: Todo.find(params[:id])
+    end
+
+    def create 
+        @todo = current_user.todos.new(todo_params)
+        if @todo.save
+          render json: @todo, include: :tags
+        else
+          render json: @todo.errors.full_messages, status: 422
+        end
+    end
+
+    def update 
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_params)
+          render json: @todo, include: :tags
+        else
+          render json: @todo.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy 
+        @todo = current_user.todos.find(params[:id])
+        @todo.destroy
+        render json: @todo, include: :tags
+    end
+
+    private 
+
+    def todo_params
+        params.require(:todo).permit(:title, :body, :done)
+    end
+end
