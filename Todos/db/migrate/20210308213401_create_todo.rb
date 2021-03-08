e660a6cbcd9f62d3a5db348f3258cce49a6e4c1b
yohan oh <yohan.oh332@gmail.com>
2021-03-08 17:12:24 -0500

    restructured folders/files

---
+++ Todos/db/migrate/20210308213401_create_todo.rb
@@ -0,0 +1,11 @@
+class CreateTodo < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :title 
+      t.string :body
+      t.boolean :done 
+
+      t.timestamps
+    end
+  end
+end
